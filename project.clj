3e969eeec773c31f6bcfc0edef2a595fa6cc6108
Eugene Koontz <ekoontz@hiro-tan.org>
2016-02-24 22:51:15 -0800

    SNAPSHOTs not allowed in releases

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/clojurescript "1.7.170"]]
-  :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+  :plugins [[cider/cider-nrepl "0.10.2"]
             [lein-doo "0.1.6"]
             [lein-cljsbuild "1.1.2"]]
 
