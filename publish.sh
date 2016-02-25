35d5ec4a78bfef35d3fec7946e1841e1115f6f2c
Eugene Koontz <ekoontz@hiro-tan.org>
2016-02-24 23:35:35 -0800

    my publish script

---
+++ publish.sh
@@ -0,0 +1,3 @@
+rsync -r ~/.m2/repository/dag_unify  ekoontz@hiro-tan.org:/var/www/mvn
+rsync -r ~/.m2/repository/babel     ec2-user@ec2-54-193-4-63.us-west-1.compute.amazonaws.com:/var/www/html/mvn
+
