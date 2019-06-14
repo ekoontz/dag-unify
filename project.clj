4a2c375b91c903673c20fa872fc8f7833e7f5fb9
Eugene Koontz <ekoontz@hiro-tan.org>
2019-06-14 14:20:36 -0400

    remove s3 stuff; not relevant

--- project.clj
+++ project.clj
@@ -5,9 +5,3 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]])
 
-  ;; run clojure tests with "lein test"
-  ;; deploy with "lein deploy s3"
-  :repositories {"s3" {:url "s3p://ekoontz-repo/releases/"
-                       :username :env/aws_access_key ;; gets environment variable AWS_ACCESS_KEY
-                       :passphrase :env/aws_secret_key}} ;; gets environment variable AWS_SECRET_KEY
-)
