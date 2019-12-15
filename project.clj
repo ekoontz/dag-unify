71f9ee3235a4dc10eb82f3f4580f9a420b83378b
Eugene Koontz <ekoontz@hiro-tan.org>
2019-12-15 20:22:02 +0100

    release 1.7.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dag_unify "1.7.2-SNAPSHOT"
+(defproject dag_unify "1.7.3"
   :description "Unification of Directed Acyclic Graphs"
   :url "https://github.com/ekoontz/dag-unify"
   :license {:name "Eclipse Public License"
