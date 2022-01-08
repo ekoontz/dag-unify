97e299c13d5b0db88c82006a6939c27268b77ba4
Eugene Koontz <ekoontz@hiro-tan.org>
2022-01-08 23:38:30 +0100

    upgrade dependencies and working on next version

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject dag_unify "1.10.0"
+(defproject dag_unify "1.10.1-SNAPSHOT"
   :description "Unification of Directed Acyclic Graphs"
   :url "https://github.com/ekoontz/dag-unify"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.2-rc1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [log4j/log4j "1.2.17"]
-                 [org.clojure/tools.logging "0.5.0"]
+                 [org.clojure/tools.logging "1.2.2"]
                  [cljslog "0.1.0"]])
