0a2c8fda26c05aa3d8f4ac390eacd1ef776d23c1
Eugene Koontz <ekoontz@hiro-tan.org>
2022-03-12 08:40:53 +0100

    Revert "update clojure to latest available"
    This reverts commit 3b4bd198ecedb0c70932741a57993695f12079ec.

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/ekoontz/dag-unify"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.4"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/tools.logging "1.2.4"]
                  [cljslog "0.1.0"]])
