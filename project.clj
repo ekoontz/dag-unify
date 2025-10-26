3fe95641a48463ee70069f4863809577cdbeea66
Eugene Koontz <ekoontz@hiro-tan.org>
2025-10-26 09:38:07 +0100

    upgrade to latest Clojure release

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/ekoontz/dag-unify"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.12.3"]
                  [org.clojure/tools.logging "1.2.4"]])
